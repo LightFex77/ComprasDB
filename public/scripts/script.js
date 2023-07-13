1086ce33f36bf544978a9fa24d9a60cd52fd2c94
LightFex77 <max706genesoviedo@gmail.com>
2023-07-13 16:40:32 -0400

    agregue el sistema de verificacion del ruc

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -2,6 +2,10 @@ const valorInput = document.getElementById("valorInput");
 const fechaVencimientoInput = document.getElementById("fechaVencimientoInput");
 const tipoProductoSelect = document.getElementById("tipo_producto");
 
+const clienteInput = document.getElementById("clienteInput");
+const botonBuscar = document.getElementById('botonBuscar');
+const clienteRuc = document.getElementById('clienteRuc');
+
 const guardarButton = document.getElementById("guardarButton");
 
 const errorMensaje = document.getElementById("errorMensaje");
@@ -21,7 +25,6 @@ function validarValor(valor) {
         return false
     }
 }
-
 function validarFecha(fecha) {
     const fechaActual = new Date();
     if (fecha >= fechaActual) {
@@ -32,7 +35,6 @@ function validarFecha(fecha) {
         return false
     }
 }
-
 function validarSelect(option){
     if(option != ""){
         errorTipo.style.display = "none"
@@ -42,8 +44,6 @@ function validarSelect(option){
         return false
     }
 }
-
-
 valorInput.addEventListener("input", function () {
     let valor = valorInput.value;
   
@@ -57,20 +57,16 @@ valorInput.addEventListener("input", function () {
     valorInput.value = valor;
     validarValor(valor)
   });
-  
-
 fechaVencimientoInput.addEventListener("change", function () {
     const fechaVencimiento = new Date(fechaVencimientoInput.value);
 
    validarFecha(fechaVencimiento)
 });
-
-
-
 tipoProductoSelect.addEventListener("change", function (){
     const selectValue = tipoProductoSelect.value;
     validarSelect(selectValue)
 })
+
 
 document.getElementById("compras").onsubmit = async function crear(e) {
     e.preventDefault()
@@ -79,7 +75,6 @@ document.getElementById("compras").onsubmit = async function crear(e) {
     const valor = form.get("valor");
     const fechaVencimiento = new Date(form.get("fecha_vencimiento"));
     const tipo = form.get("tipo_producto");
-    const fechaActual = new Date();
 
     const esValorValido = validarValor(valor);
     const esFechaValido = validarFecha(fechaVencimiento);
@@ -130,5 +125,75 @@ async function compraLista() {
 }
 //Hacer que la funcion se ejecute cada vez que se inicia la pagina
 compraLista()
+
+
+
+async function buscarCliente() {
+    const respuestaBd = await fetch("/clientes", {
+      method: "GET"
+    });
+  
+    const bdJson = await respuestaBd.json();
+    const clientesJson = bdJson.resultado;
+  
+    botonBuscar.addEventListener("click", (e) => {
+      e.preventDefault();
+      const ruc = clienteInput.value;
+  
+      let clienteEncontrado = false; // Variable para indicar si se encontró el cliente
+  
+      for (const json of clientesJson) {
+        console.log(json.ruc);
+  
+        if (json.ruc === ruc && ruc !== "") {
+          clienteRuc.style.display = "block";
+          clienteRuc.innerHTML = `
+            <span class="nombre">Nombre:</span> <p style="color: #677589">${json.nombre}</p>
+            <span class="nombre">Apellido:</span> <p style="color: #677589">${json.apellido}</p>
+          `;
+          clienteEncontrado = true; // Se encontró el cliente
+          break; // Salir del bucle
+        }
+      }
+  
+      if (!clienteEncontrado && ruc !== "") {
+        clienteRuc.style.display = "block";
+        clienteRuc.innerHTML = `
+          <span class="nombre">El cliente RUC: <b style="color: #677589">${ruc}</b> no existe<br>Desea crearlo?<br></span>
+          <button id="crearRuc">Si</button>
+          <button id="noCrearRuc">No</button>
+        `;
+  
+        const crearRuc = document.getElementById("crearRuc");
+        const noCrearRuc = document.getElementById("noCrearRuc");
+        const modal = document.getElementById("modal");
+        const rucInput = document.getElementById("ruc");
+  
+        crearRuc.addEventListener("click", (e) => {
+          e.preventDefault();
+          modal.style.opacity = 1;
+          modal.style.pointerEvents = "unset";
+          rucInput.value = ruc;
+          clienteRuc.style.display = "none";
+        });
+  
+        noCrearRuc.addEventListener("click", (e) => {
+          e.preventDefault();
+          clienteRuc.style.display = "none";
+        });
+      } else if (ruc === "") {
+        clienteRuc.style.display = "block";
+        clienteRuc.innerHTML = `
+          <span style="color:red">La casilla se encuentra vacía</span>
+        `;
+      }
+    });
+  }
+  
+  buscarCliente();
+  
+
+buscarCliente();
+
 
 document.getElementById('compras').reset();
