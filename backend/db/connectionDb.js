595d0e34b93e87922387d7b199a130a6051a0c7d
LightFex77 <max706genesoviedo@gmail.com>
2023-08-10 11:31:37 -0400

    quitar console.log

--- backend/db/connectionDb.js
+++ backend/db/connectionDb.js
@@ -1,13 +1,5 @@
 const pg = require('pg');
 
-console.log({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE,
-})
-
 const connection = new pg.Pool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
